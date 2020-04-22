ea24a510aa76bee19660a3877b71bbd6ab4ee898
gaelforget <gforget@mit.edu>
2020-04-22 11:16:24 -0400

    add RrsToChla that goes from Rrs to Chl

--- src/OceanColorData.jl
+++ src/OceanColorData.jl
@@ -1,6 +1,6 @@
 module OceanColorData
 
-export RemotelySensedReflectance
+export RemotelySensedReflectance, RrsToChla
 
 """
     RemotelySensedReflectance(rirr_in,wvbd_in,wvbd_out)
@@ -49,6 +49,38 @@ function RemotelySensedReflectance(rirr_in,wvbd_in,wvbd_out)
     end
 
     return rrs_out
+end
+
+
+"""
+    RrsToChla(Rrs)
+
+Satellite `Chl_a` estimates typicaly derive from remotely sensed reflectances
+using the blue/green reflectance ratio method and a polynomial formulation.
+
+```
+wvbd_out=Float64.([412, 443, 490, 510, 555, 670])
+wvbd_in=Float64.([400,425,450,475,500,525,550,575,600,625,650,675,700])
+rirr_in=1e-3*[23.7641,26.5037,27.9743,30.4914,28.1356,
+    21.9385,18.6545,13.5100,5.6338,3.9272,2.9621,2.1865,1.8015]
+
+rrs_out=RemotelySensedReflectance(rirr_in,wvbd_in,wvbd_out)
+chla_out=RrsToChla(rrs_out; Eqn=1)
+```
+"""
+function RrsToChla(Rrs;Eqn=1)
+    RRSB=max.(Rrs[2],Rrs[3]) #blue
+    RRSG=Rrs[5] #green
+    X = log10.(RRSB./RRSG) #ratio of blue to green
+
+    if Eqn==1
+        C=[0.3272, -2.9940, +2.7218, -1.2259, -0.5683] #OC4 algorithms (SeaWifs, CCI)
+    else
+        error("this case has not been implemented yet...")
+    end
+
+    a0=C[1]; a1=C[2]; a2=C[3]; a3=C[4]; a4=C[5];
+    chld=exp10.(a0.+a1*X+a2*X.^2+a3*X.^3+a4*X.^4); #apply polynomial recipe
 end
 
 end # module
